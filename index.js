af829a73d55854efcb4f1baa97d912ac346e673d
Bui Anh Tuan <buituann97@gmail.com>
2020-11-20 15:03:28 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Route.
+
+// app.get('/',function (req, res){
+//     return res.send("Hello word!")
+// })
+
+// Arrow funciton.
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+//127.0.0.1 - localhost
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
